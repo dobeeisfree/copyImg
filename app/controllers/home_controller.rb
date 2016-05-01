d4d30014acea666f77f40e5b8b38a2281ce6cce2
dobeeisfree <jubee0124@gmail.com>
2016-05-02 02:06:09 +0900

    간단한 인덱스 페이지 추가
    이미지 이름 보관

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
     def index
-        # image size 530 600
-        # 어디까지 했꼬 남은게 뭔지 - 점수 체크
         @pine_art = ["gogh", "mona", "pica", "scream"]
         @rand = @pine_art.sample
         @img_name = @rand + ".jpg"
